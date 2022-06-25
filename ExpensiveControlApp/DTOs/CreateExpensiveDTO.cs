0c687bfc5b970288f1fbe64d4b80b89881afc99c
lucascostasantiago <lcscostasantiago@gmail.com>
2022-06-25 07:52:40 -0300

    feat: creating expensive DTO.

---
+++ ExpensiveControlApp/DTOs/CreateExpensiveDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensiveControlApp.DTOs
+{
+    public class CreateExpensiveDTO
+    {
+
+        [Required(ErrorMessage = "Descrição é Obrigatoria.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Valor é Obrigatorio.")]
+        [Range(0.01, 999999999, ErrorMessage = "Valor deve ser maior que 0.")]
+        public double Value { get; set; }
+
+        [Required(ErrorMessage = "Data é Obrigatorio.")]
+        public DateTime Date { get; set; }
+    }
+}
